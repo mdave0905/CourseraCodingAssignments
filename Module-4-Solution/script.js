31dcb7fddcaa85778582abd3d43feb2cb32d0241
mdave0905 <50136873+mdave0905@users.noreply.github.com>
2022-02-15 22:20:55 +0530

    Update script.js

--- Module-4-Solution/script.js
+++ Module-4-Solution/script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaakov", "Jake", "Jessica", "Janice", "World", "Bro", "Cutie", "Jonathan", "Dani", "Rey"];
     
     for (var i = 0; i < names.length; i++) {
       var firstLetter = names[i].charAt(0).toLowerCase();
